f4d04bc116c3bdb5cb1f0d10a55d7cc080f065bc
Nils Daumann <nils_daumann@slindev.com>
2014-08-23 14:49:33 +0200

    added sky

--- SteamedWorlds/Classes/SWWorld.cpp
+++ SteamedWorlds/Classes/SWWorld.cpp
@@ -25,6 +25,17 @@ namespace SW
 		//Create camera with effects and stuff
 		_camera = new RN::Camera(RN::Vector2(), RN::Texture::Format::RGB16F, RN::Camera::Flags::Defaults);
 		_camera->SetBlitShader(RN::Shader::WithFile("shader/rn_DrawFramebufferTonemap"));
+		
+		RN::Model *sky = RN::Model::WithSkyCube();
+		for(int i = 0; i < sky->GetMeshCount(0); i++)
+		{
+			RN::Material *material = sky->GetMaterialAtIndex(0, i);
+			material->SetAmbientColor(RN::Color(6.0f, 6.0f, 6.0f, 1.0f));
+			material->SetOverride(RN::Material::Override::Shader);
+			material->Define("RN_ATMOSPHERE");
+		}
+		_camera->SetSky(sky);
+		
 		RN::Renderer::GetSharedInstance()->SetHDRExposure(1.0f);
 		RN::Renderer::GetSharedInstance()->SetHDRWhitePoint(2.5f);
 		
